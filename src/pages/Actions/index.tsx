d23d8686210546f9fede8eadcda1a9895d7e6e69
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2024-05-11 12:21:12 -0300

    feat: page actions of a form

---
+++ src/pages/Actions/index.tsx
@@ -0,0 +1,18 @@
+import UserForm from './components/UserForm';
+
+
+export default function Actions() {
+
+  return (
+    <>
+      <div>
+        <title>Actions</title>
+        <meta name='description' content='Add new users' />
+      </div>
+      <div>
+        <h1> 1 - Actions</h1>
+        <UserForm />
+      </div>
+    </>
+  )
+}
