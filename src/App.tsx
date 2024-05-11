bd19454cb9dc43df180ac3dedee4508aff6bab64
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2024-05-11 11:39:07 -0300

    feat: home

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
+import { useState } from 'react';
 import './App.css'
-import UserForm from './components/UserForm'
+import Routes from './Routes';
+
 function App() {
+  const [page, setPage] = useState<string>('');
+  
+  const handlePageClick = (pageName: string) => {
+    setPage(pageName);
+  };
+
+  const pageSeted = page ? true : false;
 
   return (
     <>
       <div>
         <title>Home</title>
-        <meta name='description' content='Add new users' />
+        <meta name='description' content='Homepage' />
       </div>
       <div>
-        <h1> 1 - Actions</h1>
-        <UserForm />
+          {
+            pageSeted ?
+            <Routes page={page} />
+            :
+              <ul>
+                <li>
+                  <a href="#" onClick={() => handlePageClick('actions')}>
+                    Actions
+                  </a>
+                </li>
+                <li>
+                  <a href="#" onClick={() => handlePageClick('refprops')}>
+                    RefProps
+                  </a>
+                </li>
+                <li>
+                  <a href="#" onClick={() => handlePageClick('use')}>
+                    Use
+                  </a>
+                </li>
+              </ul>
+          }
       </div>
     </>
   )
