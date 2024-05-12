aede673e9c4d963a44559c2c651556e9c378028b
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2024-05-11 23:26:08 -0300

    feat: add anothers page to render

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css'
 import Routes from './Routes';
+import { ThemeProvider } from './context/ThemeContext';
 
 function App() {
   const [page, setPage] = useState<string>('');
@@ -13,6 +14,7 @@ function App() {
 
   return (
     <>
+      <ThemeProvider>
       <div>
         <title>Home</title>
         <meta name='description' content='Homepage' />
@@ -29,18 +31,39 @@ function App() {
                   </a>
                 </li>
                 <li>
-                  <a href="#" onClick={() => handlePageClick('refprops')}>
-                    RefProps
+                  <a href="#" onClick={() => handlePageClick('refasprops')}>
+                    Ref As Props
                   </a>
                 </li>
                 <li>
-                  <a href="#" onClick={() => handlePageClick('use')}>
-                    Use
+                  <a href="#" onClick={() => handlePageClick('usetoloaddata')}>
+                    Use - to load data
+                  </a>
+                </li>
+                <li>
+                  <a href="#" onClick={() => handlePageClick('usetocontext')}>
+                    Use - to context
+                  </a>
+                </li>
+                <li>
+                  <a href="#" onClick={() => handlePageClick('useformstatus')}>
+                    UseFormStatus
+                  </a>
+                </li>
+                <li>
+                  <a href="#" onClick={() => handlePageClick('useformstate')}>
+                    UseFormState
+                  </a>
+                </li>
+                <li>
+                  <a href="#" onClick={() => handlePageClick('useoptimistic')}>
+                  UseOptimistic
                   </a>
                 </li>
               </ul>
           }
       </div>
+      </ThemeProvider>
     </>
   )
 }
