624afd14f012c440c7ed47dce240114888fe0f41
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2024-05-11 23:24:44 -0300

    feat: change route to map

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,21 +1,29 @@
 import Actions from './pages/Actions';
 import RefAsProps from './pages/RefAsProps';
 import UseToLoadData from './pages/UseToLoadData';
+import UseToContext from './pages/UseToContext';
+import UseFormStatus from './pages/UseFormStatus';
+import UseFormState from './pages/UseFormState';
+import UseOptimistic from './pages/UseOptimistic';
+
+interface pageList {
+  [key: string]: JSX.Element;
+}
+
+export const pageList: pageList = {
+  'actions': <Actions />,
+  'refasprops' : <RefAsProps />,
+  'usetoloaddata': <UseToLoadData />,
+  'usetocontext': <UseToContext />,
+  'useformstatus': <UseFormStatus />,
+  'useformstate': <UseFormState />,
+  'useoptimistic': <UseOptimistic />,
+};
 
 export default function Routes({ page }: { page: string }) {
 
   const renderPage = () => {
-
-    if (page === 'actions') {
-      return <Actions />
-    } else if (page === 'refProps') {
-      return <RefAsProps />
-    } else if (page === 'useToLoadData') {
-      return <UseToLoadData />
-    } else {
-      return <div></div>;
-    }
-   
+      return pageList[page] || <div>Page not found</div>
     }
     
 
