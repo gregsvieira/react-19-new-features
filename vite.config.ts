c8c3bead4246721353a513d0e56d323f1fb36dcd
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2024-05-21 21:32:23 -0300

    feat: add config React Compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const ReactCompilerConfig = {
+  compilationMode: "annotation",
+  sources: (filename: string | string[]) => {
+  return filename.indexOf('src/path/to/dir') !== -1;
+}, };
+
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [
+            ["babel-plugin-react-compiler", ReactCompilerConfig],
+          ],
+        },
+      }),
+    ],
+    // ...
+  };
+});
