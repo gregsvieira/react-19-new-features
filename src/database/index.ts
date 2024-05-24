f31751e68aabb37aceab03eb3c1c53e652da2806
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2024-05-23 23:26:53 -0300

    feat: add connection with database

---
+++ src/database/index.ts
@@ -0,0 +1,9 @@
+
+import { createClient } from '@supabase/supabase-js';
+import { Database } from './database.types'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const db = createClient<Database>(supabaseUrl, supabaseKey);
+
+export default db;
